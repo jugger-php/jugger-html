ab296e834fb358479b2f2e899a10a7454e6bd2bd
irpsv <i.rpsv@live.com>
2017-01-28 19:55:01 +0500

    add tests

---
+++ tests/tag-general.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * тест основных заранее созданых тэгов
+ */
+class TagGeneralTest extends TestCase
+{
+    public function testBase()
+    {
+        
+    }
+}
